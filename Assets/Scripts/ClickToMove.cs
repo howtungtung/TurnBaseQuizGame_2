48ac51cea932f76e55557fb81c9ff04b99b472f7
howtungtung <62206294+howtungtung@users.noreply.github.com>
2020-09-23 21:31:21 +0800

    完成基本滑鼠點擊測試

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour
+{
+    private Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 50))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
