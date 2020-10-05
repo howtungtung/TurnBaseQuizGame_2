13680e0bc13206ff0f03094215613ead9f7e2bdd
howtungtung <howtungtung@gmail.com>
2020-10-05 21:42:04 +0800

    update to 20200930 progress

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ClickToMove : MonoBehaviour
 {
     private Camera mainCam;
-
+    public Animator animator;
+    public NavMeshAgent agent;
+    public LayerMask groundLayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +18,15 @@ public class ClickToMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, 50))
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log(hit.transform.name);
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 50, groundLayer))
+            {
+                agent.SetDestination(hit.transform.position);
+            }
         }
+        animator.SetFloat("Speed", agent.velocity.magnitude);
     }
 }
