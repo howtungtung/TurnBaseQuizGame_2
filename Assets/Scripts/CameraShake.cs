099388c539f80af9682cacb3fd923ba4969372e7
howtungtung <howtungtung@gmail.com>
2020-10-27 23:30:46 +0800

    update to 20201021

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class CameraShake : MonoBehaviour
+{
+    private CinemachineBasicMultiChannelPerlin cinemachinePerlin;
+    public float shakeDuration = 0.2f;
+    public float shakeIntensity = 1f;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cinemachinePerlin = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    public void Shake()
+    {
+        cinemachinePerlin.m_AmplitudeGain = shakeIntensity;
+        timer = shakeDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            cinemachinePerlin.m_AmplitudeGain = Mathf.Lerp(0, shakeIntensity, timer / shakeDuration);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelEnemyBahavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnemyBahavior : MonoBehaviour
+{
+    private TransitionManager transitionManager;
+
+    private void Start()
+    {
+        transitionManager = GameObject.FindObjectOfType<TransitionManager>();    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            transitionManager.ChangeScene("BattleScene");
+        }
+    }
+}
